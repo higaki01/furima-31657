292afbc6e85754ba29114c8387011a8f03094054
higaki01 <hijiki678@gmail.com>
2020-10-24 09:09:23 +0900

    テストコード作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,14 +9,16 @@ class User < ApplicationRecord
     validates :birth_date
   end
 
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: PASSWORD_REGEX, message: '英字と数字の両方を含めて設定してください' }
+
   with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
     validates :firstname
     validates :familyname
   end
-  
-  with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: '全角文字を使用してください' } do
+
+  with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: '全角カタカナを使用してください' } do
     validates :firstname_kana
     validates :familyname_kana
   end
-
 end
