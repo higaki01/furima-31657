649c1bb062ac0bee6e0240f3fdf739f70cfa2c7d
higaki01 <hijiki678@gmail.com>
2020-10-24 19:17:12 +0900

    商品データ保存機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,22 @@
 class ItemsController < ApplicationController
+
   def index
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    binding.pry
+    Item.create(item_params)
+    render :index
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(
+      :image, :name, :description, :category_id, :condition_id, :shipping_cost_id, :prefecture_id, :shipping_day_id, :price
+    ).merge(user_id: current_user.id)
   end
 end
---
+++ app/models/item.rb
@@ -0,0 +1,10 @@
+class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :shipping_cost
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :shipping_day
+
+  has_one_attached :image
+end
