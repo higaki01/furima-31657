c52bd95198da4ca26694df3b6645045d82a1ccad
higaki01 <hijiki678@gmail.com>
2020-10-26 15:47:40 +0900

    商品詳細表示機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :items, dependent: :destroy
+  has_many :purchaces, dependent: :destroy
 
   validates :nickname, :birth_date, presence: true
 
