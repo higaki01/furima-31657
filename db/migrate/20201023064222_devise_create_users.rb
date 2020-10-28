cf4719f1b51f2c2438bc5f6d75c94ffd4eb8f371
higaki01 <hijiki678@gmail.com>
2020-10-28 17:34:51 +0900

    マイグレーション整理

--- db/migrate/20201023064222_devise_create_users.rb
+++ db/migrate/20201023064222_devise_create_users.rb
@@ -11,7 +11,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :familyname,         null: false
       t.string :firstname_kana,     null: false
       t.string :familyname_kana,    null: false
-      t.integer :birth_date,        null: false
+      t.date   :birth_date,         null: false
 
       ## Recoverable
       t.string   :reset_password_token
--- db/migrate/20201023081612_change_datatype_birth_date_of_users.rb
+++
@@ -1,5 +0,0 @@
-class ChangeDatatypeBirthDateOfUsers < ActiveRecord::Migration[6.0]
-  def change
-    change_column :users, :birth_date, :date 
-  end
-end
