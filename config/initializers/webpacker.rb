42a11113448024a8da844500aee76a8db5178683
higaki01 <hijiki678@gmail.com>
2020-10-30 05:30:07 +0900

    payjp

--- config/initializers/webpacker.rb
+++ config/initializers/webpacker.rb
@@ -1,2 +1,1 @@
 Webpacker::Compiler.env["PAYJP_PUBLIC_KEY"] = ENV["PAYJP_PUBLIC_KEY"]
-Webpacker::Compiler.env["PAYJP_PUBLIC_KEY"] = <%= Rails.application.credentials.dig(:payjp, :payjp_public_key) %>
