1968a9a30b81936fd83a3e6612fe16860c78125e
higaki01 <hijiki678@gmail.com>
2020-10-30 04:20:06 +0900

    update credentisals

--- config/initializers/webpacker.rb
+++ config/initializers/webpacker.rb
@@ -1,1 +1,2 @@
 Webpacker::Compiler.env["PAYJP_PUBLIC_KEY"] = ENV["PAYJP_PUBLIC_KEY"]
+Webpacker::Compiler.env["PAYJP_PUBLIC_KEY"] = <%= Rails.application.credentials.dig(:payjp, :payjp_public_key) %>
