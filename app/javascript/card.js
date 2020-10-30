380fc1c2d765ded881c1053b2f7ac3a8a5b89325
higaki01 <hijiki678@gmail.com>
2020-10-30 11:58:22 +0900

    Update card.js

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,6 +1,6 @@
 const pay = () => {
-  // Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
-  Payjp.setPublicKey('pk_test_a8da30208a4ab8ac3b3ae447');
+  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
+  // Payjp.setPublicKey('pk_test_a8da30208a4ab8ac3b3ae447');
   
   const form = document.getElementById('charge-form');
   form.addEventListener('submit', (e) => {
